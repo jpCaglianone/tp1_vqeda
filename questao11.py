90b068c113a2c03515198c145fb62026e3f9d013
jpCaglianone <joao.caglianone@gmail.com>
2024-11-03 03:01:34 -0300

    questao 11

--- questao11.py
+++ questao11.py
@@ -1,44 +1,61 @@
-class Pilha:
-    def __init__(self, tamanho_maximo=10):
-        self.tamanho_maximo = tamanho_maximo
-        self.itens = [None] * tamanho_maximo
-        self.topo = -1
+class Fila:
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.itens = [None] * capacidade
+        self.inicio = 0
+        self.fim = 0
+        self.tamanho = 0
 
     def is_empty(self):
-        return self.topo == -1
+        return self.tamanho == 0
 
     def is_full(self):
-        return self.topo == self.tamanho_maximo - 1
+        return self.tamanho == self.capacidade
 
-    def push(self, item):
+    def enqueue(self, item):
         if self.is_full():
-            print("Erro: A pilha está cheia")
+            print("Erro: A fila está cheia")
             return
-        self.topo += 1
-        self.itens[self.topo] = item
+        self.itens[self.fim] = item
+        self.fim += 1
+        self.tamanho += 1
+        if self.fim == self.capacidade:
+            self.fim = 0
 
-    def pop(self):
+    def dequeue(self):
         if self.is_empty():
-            print("Erro: A pilha está vazia")
+            print("Erro: A fila está vazia")
             return None
-        item = self.itens[self.topo]
-        self.itens[self.topo] = None
-        self.topo -= 1
+        item = self.itens[self.inicio]
+        self.itens[self.inicio] = None
+        self.inicio += 1
+        self.tamanho -= 1
+        if self.inicio == self.capacidade:
+            self.inicio = 0
         return item
 
     def size(self):
-        return self.topo + 1
+        return self.tamanho
 
 
 def menu ():
-    op = int(input("Digite 1 para adicionar alguem a fila, digite 2 para atender alguem , digite 3 para ver o tamanho da fila e 0 para sair"))
+    op = int(input("Digite 1 para adicionar alguem a fila, digite 2 para atender alguem , digite 3 para ver o tamanho da fila e 0 para sair: "))
     return op
 
 
 def __main__():
-    fila
+    fila = Fila(100)
     op = menu()
     while op != 0:
         if op == 1:
             nome = input("Insira o nome que deseja adicionar: ")
+            fila.enqueue(nome)
+        if op == 2:
+            print(f"Senhor(a) {fila.itens[fila.inicio]}, favor encaminhe-se para ser atendido!")
+            fila.dequeue()
+        if op == 3:
+            print(f"Ainda ha {fila.size()} pessoa(s) na fila")
 
+        op = menu()
+
+__main__()
